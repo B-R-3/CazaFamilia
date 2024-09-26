50c45015426e24cce56eaa1ffdae272ed218b727
Inedra15 <a.cachoux@gmail.com>
2024-09-26 11:01:26 +0200

    index + liste

--- CasaFamillia/Index.php
+++ CasaFamillia/Index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>CasaFamillia</title>
 </head>
+
 <body>
-    
+    <div class="fond_accueil">
+        <div class="titre">
+            <h1>Bienvenue sur la CazaFamilia !</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="log">
+            <div class="one"><a href="Liste.php">connexion</a></div>
+            <div class="sep"></div>
+            <div class="one"><a href="Liste.php">inscription</a></div>
+        </div>
+    </div>
+
+
+
+
 </body>
+
 </html>
---
+++ CasaFamillia/Liste.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Liste des produits</title>
+</head>
+<body>
+    <a href="index.php">Deconnexion</a>
+    
+    <h1>Liste des produits disponible</h1>
+    <br>
+    <br>
+    
+    <div class="liste">
+        <table>
+            <tr>
+                <th>Plats</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+            </tr>
+            <tr>
+                <td>prjrjnh</td>
+                <td>fgiehg</td>
+                <td><input type="text" name="nombre" value="nombre"></td>
+            </tr>
+            <tr>
+                <td>prjrjnh</td>
+                <td>fgiehg</td>
+                <td><input type="text" name="nombre" value="nombre"></td>
+            </tr>
+            <tr>
+                <td>prjrjnh</td>
+                <td>fgiehg</td>
+                <td><input type="text" name="nombre" value="nombre"></td>
+            </tr>
+
+           
+        </table>
+    </div>
+    <p>
+            <input type="checkbox" name="Surplace" value="oui">A emporter
+            <input type="checkbox" name="emporter" value="oui">Sur place <br><br>
+            <input type="button" name="valider" value="valider">
+        
+    </p>
+
+</body>
+</html>
