5201d5adeaef59558735918edef3b6f824199aff
B-R-3 <brandon.molinaserna@limayrac.fr>
2024-10-03 09:54:16 +0200

    liste1.0

--- CasaFamillia/Liste.php
+++ CasaFamillia/Liste.php
@@ -1,3 +1,47 @@
+<?php
+include "fonction.inc.php";
+session_start();
+
+// Redirige vers la page de connexion si on n'est pas connecté
+if (!isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit();
+}
+
+
+// Connexion à la base de données
+$dbh = connexion();
+print_r($_SESSION);
+
+$id_user = isset($_SESSION['id_user']) ? $_SESSION['id_user']: '';
+$sql = 'select libelle from produit where id_produit =:id_produit';
+try {
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array(
+        ":id_produit" => $id_produit
+    ));
+    $rows = $sth->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $ex) {
+    die("Erreur lors de la requête SQL : " . $ex->getMessage());
+}
+
+
+    $sql = "select idfaq,pseudo,question,reponse,datequestion, datereponse  from faq as F, user as U where F.iduser = U.iduser and id_produit = :id_produit;";
+    try {
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array(
+            ":id_produit" => $id_produit
+        ));
+        $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    }
+
+   
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
