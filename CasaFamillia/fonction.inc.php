c06fbce7ff6c8ed0c3b55cef97968a9a63811295
RomainCaner <romain.caner@limayrac.fr>
2024-10-03 09:08:48 +0200

    fonction.php /connextion a la bdd

---
+++ CasaFamillia/fonction.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+function connexion() {
+  $dsn = 'mysql:host=localhost;dbname=caza familia';
+  $user = 'root';
+  $password = '';
+  
+  try {
+    $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND =>
+    "SET NAMES utf8"));
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $ex) {
+    die("Erreur lors de la connexion SQL : " . $ex->getMessage());
+    }
+    return $dbh;
+  }
+
+function deconnexion(){
+  //$dbh = NULL;
+  unset($dbh);
+}
+?>
