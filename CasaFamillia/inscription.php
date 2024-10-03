72b852cbc76dd2c7eec8b9b6b0022f8285a1406f
RomainCaner <romain.caner@limayrac.fr>
2024-10-03 09:33:27 +0200

    Incription Réalisée !

--- CasaFamillia/inscription.php
+++ CasaFamillia/inscription.php
@@ -1,3 +1,33 @@
+<?php
+include "fonction.inc.php";
+
+session_start();
+
+if(array_key_exists('Valider', $_POST)) {
+    if(isset($_POST["login"]) && isset($_POST["mot_de_passe"]) && isset($_POST["email"])){
+        setUser($_POST["login"],$_POST["mot_de_passe"],$_POST["email"]);
+    }
+}
+
+function setUser($log,$mdp,$mail){
+    
+    // Connexion à la base de données
+    $dbh = connexion();
+    $sql = "INSERT INTO _user (login, mot_de_passe, email) VALUES (:login, :mot_de_passe, :email)";
+
+    try {
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array(
+        ':login' => $log,
+        ':mot_de_passe' => $mdp,
+        ':email' => $mail,
+    ));
+    } catch (PDOException $ex) {
+    die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    }
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -18,14 +48,14 @@
 
 <form method="POST">
 
-    <label for="Pseudo" >Pseudo</label><br>
-    <input type="text" id="Pseudo" name="Pseudo" /><br>
+    <label for="login" >Pseudo</label><br>
+    <input type="text" id="login" name="login" /><br>
 
-    <label for="mdp" >Mots de passe</label><br>
-    <input type="password" id="Mdp" name="Mdp"/><br>
+    <label for="mot_de_passe" >Mots de passe</label><br>
+    <input type="password" id="mot_de_passe" name="mot_de_passe"/><br>
 
     <label for="email" >Email</label><br>
-    <input type="text" id="Mail" name="Mail"/><br><br>
+    <input type="text" id="email" name="email"/><br><br>
 
     <button type="submit" name="Valider">Valider</button><br><br>
 
