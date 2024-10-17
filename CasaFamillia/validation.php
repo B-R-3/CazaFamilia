48706054f7700a1c7ccb906c2257ae4da8a16300
RomainCaner <romain.caner@limayrac.fr>
2024-10-17 09:17:09 +0200

    Validation

--- CasaFamillia/validation.php
+++ CasaFamillia/validation.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 include "fonction.inc.php";
-print_r($_SESSION);
-print_r($_GET);
 
 // Connexion à la base de données
 $dbh = connexion();
@@ -21,11 +19,12 @@ $id_commande = isset($_GET["id_commande"]) ? $_GET["id_commande"] : '';
 $qte = isset($_POST['qte']) ? $_POST['qte'] : array(); // Capture les quantités
 $produits_commande = array(); // Initialisation à un tableau vide
 $total_commande = 0; // Initialisation à 0
+$total_ligne_ht = 0;
 
 // Récupérer les produits commandés de la base de données
 // Exemple : SELECT produit, prix_ht FROM produits WHERE id_user = :id_user
 // Simulation des produits (ceci doit être remplacé par votre logique réelle)
-$sql = 'SELECT p.libelle, p.prix_ht, l.qte  FROM lignecommande as l, produit as p where l.id_produit = p.id_produit and id_commande = :id_commande';
+$sql = 'SELECT p.libelle, p.prix_ht, l.qte, c.total_commande FROM lignecommande AS l, produit AS p, commande AS c WHERE l.id_produit = p.id_produit AND l.id_commande = c.id_commande AND c.id_commande = :id_commande;';
 try {
     $sth = $dbh->prepare($sql);
     $sth->execute(
@@ -68,13 +67,12 @@ try {
                 <td><?php echo htmlspecialchars($row['libelle']); ?></td>
                 <td><?php echo number_format($row['prix_ht'], 2); ?> €</td>
                 <td><?php echo htmlspecialchars($row['qte']); ?></td>
-                <!-- <td><?php //echo htmlspecialchars($row['prix']); 
-                            ?></td> -->
+                <td><?php echo htmlspecialchars($row['prix_ht']*$row['qte']);?> € </td>
             </tr>
         <?php endforeach; ?>
     </table>
 
-    <h3>Total de la commande (HT) : <?php echo number_format($total_commande, 2); ?> €</h3>
+    <h3>Total de la commande  : <?php echo number_format($row['total_commande'], 2); ?> €</h3>
 
     <p><a href="index.php">Retour à l'accueil</a></p>
     <p><a href="payement.php?id_commande=<?php echo $id_commande; ?>">Régler la commande</a></p>
