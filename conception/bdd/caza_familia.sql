6d5767cb7424dcc6d0983c86a563247acc50c115
RomainCaner <romain.caner@limayrac.fr>
2024-11-07 11:45:58 +0100

    TEST

--- conception/bdd/caza_familia.sql
+++ conception/bdd/caza_familia.sql
@@ -24,6 +24,22 @@ CREATE DATABASE IF NOT EXISTS `caza familia` DEFAULT CHARACTER SET utf8 COLLATE 
 USE `caza familia`;
 
 -- --------------------------------------------------------
+DELIMITER $$
+CREATE DEFINER=root@localhost FUNCTION renvoie_somme_produit (idcom INT) RETURNS INT(11) DETERMINISTIC BEGIN
+    DECLARE total_qte INT;
+    
+    -- Calcul de la somme des quantités pour une commande donnée
+    SELECT SUM(qte) INTO total_qte
+    FROM lignecommande
+    WHERE id_commande = idcom;
+    
+    -- Retourner la somme calculée
+    RETURN total_qte;
+END$$
+
+DELIMITER ;
+
+
 
 --
 -- Structure de la table `commande`
@@ -162,7 +178,7 @@ INSERT INTO `produit` (`id_produit`, `libelle`, `prix_ht`, `quantité_produit`) 
 (1, 'Tomate Mozzarella Basilic ', 10.00, 15),
 (2, 'Carpaccio de bresaola au pesto', 12.00, 15),
 (3, 'Bruschetta Tomate Jambon', 12.00, 17),
-(4, 'Wrap à l\'italienne', 8.00, 18),
+(4, "Wrap à l'italienne", 8.00, 18),
 (5, 'Lasagnes bolognaise', 15.00, 20),
 (6, 'Cannelloni fromage/bolognaise', 14.00, 25),
 (7, 'Spaghetti Bolognaise/Carbonara', 14.00, 22),
