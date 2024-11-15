3b4999b857ed501b5ee1fcda2926f4c6d7f926ba
Brandon.molina <158175112+B-R-3@users.noreply.github.com>
2024-11-15 02:59:38 +0100

    Add files via upload

--- CasaFamillia/confirmation.php
+++ CasaFamillia/confirmation.php
@@ -1,21 +1,28 @@
-<?php
-include "fonction.inc.php";
-session_start();
-?>
+        
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Confirmation de Paiement</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
+    <title>Menu</title>
 </head>
+<div class="space"></div>
 <body>
-    <h1>Merci pour votre commande !</h1>
+    <div class="suite">
+
+        <h1>Merci pour votre commande !</h1>
     <p>Votre paiement a été confirmé avec succès.</p>
     
     <p>Nous préparons votre commande avec soin et elle sera prête sous peu.</p>
     
-    <a href="index.php"><button>Retour à l'accueil</button></a>
+    <a href="index.php"><button class="wave-button">Retour à l'accueil</button></a>
+    </div>
+
 </body>
+
 </html>
